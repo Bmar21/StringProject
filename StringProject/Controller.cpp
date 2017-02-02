d7dc07ba723e612ee4b0311095d81faa11758e31
Bmar21 <brandonmalor21@gmail.com>
2017-02-02 13:02:25 -0700

    Created new C++ project for strings

---
+++ StringProject/Controller.cpp
@@ -0,0 +1,9 @@
+//
+//  Controller.cpp
+//  StringProject
+//
+//  Created by Marlor, Brandon on 2/2/17.
+//  Copyright © 2017 Marlor, Brandon. All rights reserved.
+//
+
+#include "Controller.hpp"
---
+++ StringProject/Controller.hpp
@@ -0,0 +1,14 @@
+//
+//  Controller.hpp
+//  StringProject
+//
+//  Created by Marlor, Brandon on 2/2/17.
+//  Copyright © 2017 Marlor, Brandon. All rights reserved.
+//
+
+#ifndef Controller_hpp
+#define Controller_hpp
+
+#include <stdio.h>
+
+#endif /* Controller_hpp */
